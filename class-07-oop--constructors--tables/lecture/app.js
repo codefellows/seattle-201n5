921bcb5509f709f3813ae33f0bf0516629e85741
Dan Schwartz <dpschwartz@gmail.com>
2016-06-11 10:22:46 -0700

    Add constructor demo to class07

--- class-07-oop--constructors--tables/lecture/app.js
+++ class-07-oop--constructors--tables/lecture/app.js
@@ -1,21 +1,55 @@
-var shoes = {
-  name: 'shoes',
-  price: 49.99,
-  tax: 4.75,
-  total: 54.74
+var allItems = [];
+
+function Item(n, price) {
+  this.name = n;
+  this.price = price;
+  this.tax = 0;
+  this.total = 0;
+  allItems.push(this);
+}
+
+Item.prototype.calcTotal = function () {
+  return this.total = parseFloat((this.price + this.tax).toFixed(2));
 };
 
-var socks = {
-  name: 'socks',
-  price: 8.99,
-  tax: 0,
-  total: 0,
-
-  calcTax: function() {
-    this.tax = this.price * 0.095;
-  },
-
-  calcTotal: function() {
-    return this.total = this.price + this.tax;
-  }
+Item.prototype.calcTax = function() {
+  this.tax = parseFloat((this.price * 0.095).toFixed(2));
 };
+
+Item.prototype.doAllTheMethods = function() {
+  this.calcTax();
+  this.calcTotal();
+};
+
+var socks = new Item('socks', 8.99);
+var shoes = new Item('shoes', 49.99);
+var pantaloons = new Item('pantaloons', 89.99);
+
+socks.doAllTheMethods();
+
+shoes.calcTax();
+shoes.calcTotal();
+
+pantaloons.calcTax();
+pantaloons.calcTotal();
+
+//get table
+var table = document.getElementById('generated-table');
+
+function makeRow(obj) {
+  //make a row
+  var row = document.createElement('tr');
+
+  //REPEAT THIS PART
+    //make a cell
+    var cell = document.createElement('td');
+    //give content to cell
+    cell.textContent = obj.name;
+    //append cell to the row
+    row.appendChild(cell);
+
+  //append row to the table
+  table.appendChild(row);
+}
+
+makeRow(socks);
